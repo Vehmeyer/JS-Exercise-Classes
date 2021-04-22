f587f9eef4f01e2ed1a3a2f48ea820b1633e1cdf
John Vehmeyer <j.vehmeyer@gmail.com>
2021-04-22 14:06:23 -0500

    completed task 1

--- index.js
+++ index.js
@@ -45,6 +45,21 @@ class Airplane {
     constructor(name, age) {
       this.name = name;
       this.age = age;
+      this.stomach = [];
+    }
+  
+    eat(edible){
+      if(this.stomach.length < 10){
+        this.stomach.push(edible);
+      }
+    }
+
+    poop(){
+      this.stomach = [];
+    }
+
+    toString(){
+    return `${this.name}, ${this.age}`;
     }
   }
   
